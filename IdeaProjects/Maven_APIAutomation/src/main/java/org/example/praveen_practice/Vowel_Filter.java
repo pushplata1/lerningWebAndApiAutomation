2a9e223293e750b58665e21bf01be076a5038716
Praveen_Test <praveen14jnt@gmail.com>
2025-01-13 23:59:53 +0530

    Added the new method Vowel for print out of String of vowel

--- IdeaProjects/Maven_APIAutomation/src/main/java/org/example/praveen_practice/Vowel_Filter.java
+++ IdeaProjects/Maven_APIAutomation/src/main/java/org/example/praveen_practice/Vowel_Filter.java
@@ -5,8 +5,21 @@ import java.util.List;
 
 public class Vowel_Filter {
 
-    public List<Character> Vowel_List(String s){
+    public void Vowel(String s){
+        StringBuilder b=new StringBuilder();
+        for(int i=0;i<s.length();i++){
+            char chr =s.charAt(i);
+            if (chr=='a'||chr=='e'||chr=='i'||chr=='o'||chr=='u'){
+               b.append(chr).append(" ");
+            }
+
+        } System.out.println("String of filtered vowel characters from string: "+b.toString().trim());
+
+    }
+
+    public void Vowel_List(String s){
         List <Character> k=new ArrayList<>();
+
         for(int i=0;i<s.length();i++){
             char chr =s.charAt(i);
             if (chr=='a'||chr=='e'||chr=='i'||chr=='o'||chr=='u'){
@@ -14,14 +27,15 @@ public class Vowel_Filter {
             }
 
         }
-        return  k;
+        System.out.println("List of filtered vowels characters from string "+k);
     }
 
 
     public static void main(String [] args) {
         String s="Praveen";
        Vowel_Filter v =new Vowel_Filter();
-    System.out.println(v.Vowel_List(s));
+    v.Vowel_List(s);
+    v.Vowel(s);
 
 
     }
